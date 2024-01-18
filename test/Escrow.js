ad54a2c7b57f3bbf863eca935b199d7173c19dda
kellyn <kellynwong@hotmail.com>
2024-01-18 14:44:34 +0800

    Mint NFT and deploy Escrow contract

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,10 +6,33 @@ const tokens = (n) => {
 };
 
 describe("Escrow", () => {
-  it("saves the addresses"),
-    async () => {
-      const RealEstate = await ethers.getContractFactory("RealEstate");
-      realEstate = await RealEstate.deploy();
-      console.log(realEstate.address);
-    };
+  let buyer, seller, inspector, lender;
+  let realEstate, escrow;
+
+  it("saves the addresses", async () => {
+    // Setup accounts
+    [buyer, seller, inspector, lender] = await ethers.getSigners();
+
+    // Deploy Real Estate contract
+    const RealEstate = await ethers.getContractFactory("RealEstate");
+    realEstate = await RealEstate.deploy();
+    // console.log(realEstate.address);
+
+    // Mint
+    let transaction = await realEstate
+      .connect(seller)
+      .mint(
+        "https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS"
+      );
+    await transaction.wait();
+
+    // Deploy Escrow contract
+    const Escrow = await ethers.getContractFactory("Escrow");
+    escrow = await Escrow.deploy(
+      realEstate.address,
+      seller.address,
+      inspector.address,
+      lender.address
+    );
+  });
 });
