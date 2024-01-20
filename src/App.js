cb8da27c65698e28c58f5f663e5c5da47f108aa2
kellyn <kellynwong@hotmail.com>
2024-01-21 01:26:46 +0800

    Create placeholder for cards

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ function App() {
 
   // Update page if you change metamask account
   window.ethereum.on("accountsChanged", async () => {
+    // Refetch accounts
     const accounts = await window.ethereum.request({
       method: "eth_requestAccounts",
     });
@@ -42,8 +43,25 @@ function App() {
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
+      <Search />
       <div className="cards__section">
-        <h3>Welcome to Millow!</h3>
+        <h3>Homes for you</h3>
+        <hr />
+        <div className="cards">
+          <div className="card">
+            <div className="card__image">
+              <img src="" alt="Home"></img>
+            </div>
+            <div className="card__info">
+              <h4>1 ETH</h4>
+              <p>
+                <strong>1</strong>bds |<strong>2</strong>ba |<strong>3</strong>
+                sqft
+              </p>
+              <p>1234 Elm Street</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
